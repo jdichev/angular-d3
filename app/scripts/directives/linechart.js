6d02309f300a2ee379bcf7a3bf6475e9825c0ff8
jdichev <jordan@dichev.net>
2014-03-10 00:36:47 +0200

    Multi-series enabled

--- app/scripts/directives/linechart.js
+++ app/scripts/directives/linechart.js
@@ -18,69 +18,86 @@ angular.module('angular-D3-lineChart', [])
         });
 
         function go() {
-          var margin = {
+          var margins = {
               top: 20,
               right: 20,
               bottom: 30,
               left: 50
             },
-            width = element.width() - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
 
-          var parseDate = d3.time.format('%Y-%m').parse;
+            width = element.width() - margins.left - margins.right,
 
-          var x = d3.time.scale()
-            .range([0, width]);
+            height = 200 - margins.top - margins.bottom,
 
-          var y = d3.scale.linear()
-            .range([height, 0]);
+            color = d3.scale.category10(),
 
-          var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient('bottom');
+            parseDate = d3.time.format('%Y-%m').parse,
 
-          var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient('left');
+            x = d3.time.scale()
+              .range([0, width]),
 
-          var line = d3.svg.line()
-            .x(function (d) {
-              return x(d.date);
+            y = d3.scale.linear()
+              .range([height, 0]),
+
+            xAxis = d3.svg.axis()
+              .scale(x)
+              .orient('bottom'),
+
+            yAxis = d3.svg.axis()
+              .scale(y)
+              .orient('left'),
+
+            line = d3.svg.line()
+              // .interpolate('basis')
+              .x(function (d) {
+                return x(d.date);
+              })
+              .y(function (d) {
+                return y(d.value);
+              }),
+
+            svg = d3.select(element[0]).append('svg')
+              .attr('width', width + margins.left + margins.right)
+              .attr('height', height + margins.top + margins.bottom)
+              .append('g')
+              .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
+
+          var data = [];
+          for (var i = 1, dataHeaderLength = scope.lineChartData[0].length; i < dataHeaderLength; i += 1) {
+            var colData = {
+              name: scope.lineChartData[0][i],
+              values: []
+            }
+            for (var j = 1, dataLength = scope.lineChartData.length; j < dataLength; j += 1) {
+              colData.values.push({
+                date: parseDate(scope.lineChartData[j][0]),
+                value: scope.lineChartData[j][i]
+              });
+            }
+            data.push(colData);
+          }
+
+          x.domain(d3.extent(scope.lineChartData, function (d) {
+            return parseDate(d[0]);
+          }));
+
+          y.domain([
+            d3.min(data, function (c) {
+              return d3.min(c.values, function (v) {
+                return v.value;
+              });
+            }),
+            d3.max(data, function (c) {
+              return d3.max(c.values, function (v) {
+                return v.value;
+              });
             })
-            .y(function (d) {
-              return y(d.value);
-            });
-
-          var svg = d3.select(element[0]).append('svg')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-            .append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-
-          var data = scope.lineChartData.map(function (d) {
-            return {
-              date: parseDate(d[0]),
-              value: d[1]
-            };
-          });
-
-          x.domain(d3.extent(data, function (d) {
-            return d.date;
-          }));
-
-          y.domain(d3.extent(data, function (d) {
-            return d.value;
-          }));
+          ]);
 
           svg.append('g')
             .attr('class', 'x axis')
             .attr('transform', 'translate(0,' + height + ')')
-            .call(xAxis)
-            .append('text')
-            .text('x axis')
-            .style('text-anchor', 'end')
-            .attr('transform', 'translate(' + width + ', -10)');
+            .call(xAxis);
 
           svg.append('g')
             .attr('class', 'y axis')
@@ -90,17 +107,19 @@ angular.module('angular-D3-lineChart', [])
             .attr('y', 6)
             .attr('dy', '.71em')
             .style('text-anchor', 'end')
-            .text('y')
-            .attr('transform', 'rotate(0)')
-            .attr('transform', 'translate(20, 0)')
-          ;
+            .text('values');
 
-          svg.append('path')
-            .datum(data)
+          var column = svg.selectAll('.data-column')
+            .data(data)
+            .enter().append('g')
+            .attr('class', 'data-column');
+
+          column.append('path')
             .attr('class', 'line')
-            .attr('d', line);
+            .attr('d', function(d) { return line(d.values); })
+            .style('stroke', function(d) { return color(d.name); });
 
-        }
+        } // end go()
 
       }
     };
